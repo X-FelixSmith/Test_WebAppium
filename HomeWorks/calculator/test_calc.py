7608970c72ac4892d8d64e449b0d20522949385a
ck <2244200475@qq.com>
2021-04-04 13:03:09 +0800

    setup里实例化

--- HomeWorks/calculator/test_calc.py
+++ HomeWorks/calculator/test_calc.py
@@ -10,6 +10,7 @@ class TestCalc():
     print(data)
 
     def setup(self):
+        self.calc = Calculator()
         print("开始计算")
 
     def teardown(self):
@@ -19,23 +20,20 @@ class TestCalc():
     @pytest.mark.parametrize("a,b,expect", data["add_int"]["datas"], ids=data["add_int"]["ids"])
     def test_add_int(self, a, b, expect):
         print("计算整数加法")
-        calc = Calculator()
-        assert calc.add(a, b) == expect
+        assert self.calc.add(a, b) == expect
 
     # 浮点数 加法
     @pytest.mark.parametrize("a,b,expect", data["add_float"]["datas"], ids=data["add_float"]["ids"])
     def test_add_float(self,a,b,expect):
-        calc = Calculator()
         print("计算浮点数加法")
-        assert round(calc.add(a, b),2) == expect
+        assert round(self.calc.add(a, b),2) == expect
 
     # 整数除法
     @pytest.mark.parametrize("a,b,expect", data["div_datas"]["datas"], ids=data["div_datas"]["ids"])
     def test_division(self, a, b, expect):
-        calc = Calculator()
         print("计算整数除法")
         try:
-            assert calc.division(a, b) == expect
+            assert self.calc.division(a, b) == expect
 
         except ZeroDivisionError as e:
             print("除数为零")
