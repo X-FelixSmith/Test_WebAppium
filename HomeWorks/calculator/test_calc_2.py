6b7999e66e1653a53f68a973d67106b99da7c7f1
ck <2244200475@qq.com>
2021-04-04 21:36:17 +0800

    fixture 实现setup/teardown 功能,实现 参数化的功能,生成测试 报告

--- HomeWorks/calculator/test_calc_2.py
+++ HomeWorks/calculator/test_calc_2.py
@@ -8,21 +8,18 @@ from calculator import Calculator
 class TestCalc():
 
     # 整数 加法
-    # @pytest.mark.parametrize("a,b,expect", data["add_int"]["datas"], ids=data["add_int"]["ids"])
     @allure.story("整数相加功能")
     def test_add_int(self, get_calc,data_add_int):
         print("计算整数加法")
         assert get_calc.add(data_add_int[0],data_add_int[1]) == data_add_int[2]
 
     # 浮点数 加法
-    # @pytest.mark.parametrize("a,b,expect", data["add_float"]["datas"], ids=data["add_float"]["ids"])
     @allure.story("浮点数相加功能")
     def test_add_float(self,get_calc,data_add_float):
         print("计算浮点数加法")
         assert round(get_calc.add(data_add_float[0],data_add_float[1]),2) == data_add_float[2]
 
     # 整数除法
-    # @pytest.mark.parametrize("a,b,expect", data["div_datas"]["datas"], ids=data["div_datas"]["ids"])
     @allure.story("除法功能")
     def test_division(self, get_calc,data_div):
         print("计算整数除法")
