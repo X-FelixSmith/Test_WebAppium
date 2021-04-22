140808e96b45c2b88892ed37c2c5db02117f7d95
ck <2244200475@qq.com>
2021-04-22 18:30:05 +0800

    update 点击添加成员按钮

--- HomeWorks/test_po/package/contact_page.py
+++ HomeWorks/test_po/package/contact_page.py
@@ -10,9 +10,9 @@ from HomeWorks.test_po.package.basepage import BasePage
 class ContactPage(BasePage):
     # 点击添加成员
     def click_add_member(self):
-
+        # 添加成员按钮需要确定一个进行点击，否则有几率报错
         while True:
-            self.find_and_click(By.CSS_SELECTOR, ".ww_operationBar .js_add_member")
+            self.find_and_click(By.CSS_SELECTOR, ".ww_operationBar:nth-child(1)>.js_add_member")
             ele_num = len(self.finds(By.ID, "username"))
             if ele_num > 0:
                 break
@@ -20,7 +20,7 @@ class ContactPage(BasePage):
 
     # 得到成员信息
     def get_member(self):
-        sleep(1)
+        sleep(2)
         member_list = []
         ele_phones = self.finds(By.CSS_SELECTOR, ".member_colRight_memberTable_td:nth-child(5)")
         for value in ele_phones:
